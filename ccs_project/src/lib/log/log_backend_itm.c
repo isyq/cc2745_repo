e03801a1f8d021b9c7131e2a8375d2b79c83a37f
isyq <syq@outlook.com>
2026-04-15 13:24:08 +0800

    Add itm log
    No message

--- ccs_project/src/lib/log/log_backend_itm.c
+++ ccs_project/src/lib/log/log_backend_itm.c
@@ -1,5 +1,7 @@
 #include <stdbool.h>
-#include <ti\log\Log.h>
+#include <ti/drivers/dpl/HwiP.h>
+#include <ti/drivers/ITM.h>
+#include <ti/log/LogSinkITM.h>
 
 static bool m_enabled;
 
@@ -8,13 +10,33 @@ void log_backend_itm_init(void)
     if (!m_enabled)
     {
         m_enabled = true;
+
+        // LogSinkITM_init();
     }
 }
 
 // FIXME: Log_printf does not support this usage
 void log_backend_itm_write(uint8_t* p_data, uint16_t data_len)
 {
-    p_data[data_len] = '\0';
+    uint32_t key;
 
-    Log_printf(LogModule0, Log_INFO, p_data);
+    key = HwiP_disable();
+
+    // ITM_send32Polling(LogSinkITM_STIM_HEADER, headerPtr);
+
+    for (uint16_t i = 0; i < data_len; ++i)
+    {
+        ITM_send8Polling(LogSinkITM_STIM_TRACE, p_data[i]);
+    }
+
+    HwiP_restore(key);
+}
+
+void log_backend_uninit(void)
+{
+    uint32_t key = HwiP_disable();
+
+    ITM_close();
+
+    HwiP_restore(key);
 }
