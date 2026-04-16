f2d1759f75306bd7162ef83326017d7dbc2349cd
isyq <syq@outlook.com>
2026-04-16 11:53:00 +0800

    Add freertos exception callback
    No message

--- ccs_project/src/module/framework/fwk_hook.c
+++ ccs_project/src/module/framework/fwk_hook.c
@@ -2,9 +2,16 @@
 #include <task.h>
 #include "ti/ble/stack_util/health_toolkit/assert.h"
 #include "fwk_init.h"
+#include "log.h"
 
 void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName)
 {
     //Handle FreeRTOS Stack Overflow
     fwk_assert_handler(HAL_ASSERT_CAUSE_STACK_OVERFLOW_ERROR, 0);
+}
+
+void freertos_hardfault_callback(void)
+{
+    for (;;)
+    {;}
 }
--- ccs_project/src/runner/runner_iic.c
+++ ccs_project/src/runner/runner_iic.c
@@ -11,15 +11,6 @@ static void runner_task_entry(void* arg)
 
     for (;;)
     {
-        if (count < 1)
-        {
-            hal_iic_close(IIC_INST);
-            hal_iic_send(IIC_INST, 0x1122, (uint8_t*)"Hello\r\n", 7);
-        }
-        else
-        {
-            hal_iic_init(IIC_INST, 1, NULL);
-        }
         hal_iic_send(IIC_INST, 0x1122, (uint8_t*)"Hello\r\n", 7);
 
         log_info("IIC count, %d", count++);
